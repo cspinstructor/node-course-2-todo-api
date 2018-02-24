581e9d406d1975d83917622e51471e1d78d5c431
paulchin <paulchin20@yahoo.com>
2018-02-24 23:59:16 +0800

    Add update script

---
+++ playground/mongodb-update.js
@@ -0,0 +1,39 @@
+// const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, client) => {
+  if (err) {
+    return console.log('Unable to connect to MongoDB server');
+  }
+  console.log('Connected to MongoDB server');
+  const db = client.db('TodoApp');
+
+  db.collection('Todos').findOneAndUpdate({
+    _id: new ObjectID('5a9181f5c1a78a67b492e9e4')
+  }, {
+    $set: {
+      completed: true
+    }
+  }, {
+    returnOriginal: false
+  }).then((result) => {
+    console.log(result);
+  });
+
+  db.collection('Users').findOneAndUpdate({
+    _id: new ObjectID('5a91416436e117148431cbee')
+  }, {
+    $set: {
+      name: 'Darren'
+    },
+    $inc: {
+      age: 1
+    }
+  }, {
+    returnOriginal: false
+  }).then((result) => {
+    console.log(result);
+  });
+
+  //client.close();
+});
