66c148ac7e98ee7910cdbcf55cc34e72ce43ca28
paulchin <paulchin20@yahoo.com>
2018-02-26 15:32:14 +0800

    Add DELETE /todos/:id

--- playground/mongodb-delete.js
+++ playground/mongodb-delete.js
@@ -1,12 +1,11 @@
 // const MongoClient = require('mongodb').MongoClient;
 const {MongoClient, ObjectID} = require('mongodb');
 
-MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, client) => {
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
   if (err) {
     return console.log('Unable to connect to MongoDB server');
   }
   console.log('Connected to MongoDB server');
-  const db = client.db('TodoApp');
 
   // deleteMany
   // db.collection('Todos').deleteMany({text: 'Eat lunch'}).then((result) => {
@@ -23,7 +22,7 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, client) => {
   //   console.log(result);
   // });
 
-  db.collection('Users').deleteMany({name: 'Andrew'});
+  // db.collection('Users').deleteMany({name: 'Andrew'});
 
   db.collection('Users').findOneAndDelete({
     _id: new ObjectID("57ac8d47878a299e5dc21bc8")
@@ -31,5 +30,5 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, client) => {
     console.log(JSON.stringify(results, undefined, 2));
   });
 
-  // client.close();
+  // db.close();
 });
